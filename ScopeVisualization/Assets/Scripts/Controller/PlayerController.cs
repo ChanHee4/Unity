673d92e1b18515d9fb74b78dd33d05d27a5f1192
admin <>
2023-04-27 13:36:35 +0900

    112233

---
+++ ScopeVisualization/Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private GameObject Parent;
+    
+
+    private string EnemyName = "Enemy";
+
+    
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Instantiate(PrefabManager.GetInstance.getPrefabByName("EnemyName")).transform.SetParent(Parent.transform);
+            
+        }
+    }
+}
