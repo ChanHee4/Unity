e434af8ceb21e3effd1276b33940081374399cf4
admin <>
2023-05-03 13:43:51 +0900

    14561456

---
+++ WIN32API_Framework/WIN32API_Framework/MainUpdate.cpp
@@ -0,0 +1,62 @@
+#include "MainUpdate.h"
+#include "Player.h"
+#include "Enemy.h"
+
+
+MainUpdate::MainUpdate() : m_pPlayer(NULL)
+{
+
+}
+
+MainUpdate::~MainUpdate()
+{
+	Destroy();
+}
+
+
+void MainUpdate::Start()
+{
+	m_hdc = GetDC(g_hWnd);
+
+	m_pPlayer = new Player();
+	m_pPlayer->Start();
+
+	m_pEnemy = new Enemy();
+	m_pEnemy->Start();
+}
+
+void MainUpdate::Update()
+{
+	if(m_pPlayer)
+		m_pPlayer->Update();
+
+	if (m_pEnemy)
+		m_pEnemy->Update();
+}
+
+void MainUpdate::Render()
+{
+	Rectangle(m_hdc, 0, 0, WIDTH, HEIGHT);
+
+	if (m_pPlayer)
+		m_pPlayer->Render(m_hdc);
+
+	if (m_pEnemy)
+		m_pEnemy->Render(m_hdc);
+
+}
+
+void MainUpdate::Destroy()
+{
+	if (m_pPlayer)
+	{
+		delete m_pPlayer;
+		m_pPlayer = NULL;
+	}
+
+	if (m_pEnemy)
+	{
+		delete m_pEnemy;
+		m_pEnemy = NULL;
+	}
+}
---
+++ WIN32API_Framework/WIN32API_Framework/MainUpdate.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Include.h"
+
+class GameObject;
+class MainUpdate
+{
+private:
+	HDC m_hdc;
+
+	GameObject* m_pPlayer;
+	GameObject* m_pEnemy;
+public:
+	void Start();
+	void Update();
+	void Render();
+	void Destroy();
+public:
+	MainUpdate();
+	~MainUpdate();
+};
+
