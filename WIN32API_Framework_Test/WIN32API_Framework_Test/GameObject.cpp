63714f30a33ded850315443ea7a6e107e6e84448
admin <>
2023-05-12 13:43:03 +0900

    123987

---
+++ WIN32API_Framework_Test/WIN32API_Framework_Test/GameObject.cpp
@@ -0,0 +1,12 @@
+#include "GameObject.h"
+
+
+GameObject::GameObject() : Speed(0.0f)
+{
+
+}
+
+GameObject::~GameObject()
+{
+
+}
---
+++ WIN32API_Framework_Test/WIN32API_Framework_Test/Include.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <vector>
+#include <list>
+#include <map>
+#include <string>
+#include <Windows.h>
+#include <math.h>
+
+
+using namespace std;
+
+
+#include "Define.h"
+#include "Structs.h"
+#include "Enum.h"
