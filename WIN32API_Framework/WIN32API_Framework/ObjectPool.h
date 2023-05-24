6c310ce9d5d2e1baf1fa8364d04a99ab94ccc2dd
admin <>
2023-05-24 13:45:45 +0900

    123654

---
+++ WIN32API_Framework/WIN32API_Framework/ObjectPool.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "Include.h"
+
+class GameObject;
+class ObjectPool
+{
+public:
+	Single(ObjectPool)
+private:
+	map<string, list<GameObject*>> PoolList;
+public:
+	list<GameObject*>* GetList(string _key) 
+	{
+		map<string, list<GameObject*>>::iterator iter = PoolList.find(_key);
+
+		if (iter == PoolList.end())
+			return nullptr;
+		
+		return &iter->second;
+	}
+
+	GameObject* GetGameObject(string _key)
+	{
+		list<GameObject*>* tempList = GetList(_key);
+
+		if (tempList == nullptr)
+			return nullptr;
+
+		GameObject* Obj = tempList->front();
+		tempList->pop_front();
+
+		return Obj;
+	}
+
+	void ReturnObject(GameObject* _Object);
+private:
+	ObjectPool();
+public:
+	~ObjectPool();
+};
